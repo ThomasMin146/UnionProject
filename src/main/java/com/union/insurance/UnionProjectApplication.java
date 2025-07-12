f93ebae0196bbae6add8c003c3865a5f974ac109
thomas <minarovic.thomas@gmail.com>
2025-07-12 13:15:51 +0200

    init commit

---
+++ src/main/java/com/union/insurance/UnionProjectApplication.java
@@ -0,0 +1,13 @@
+package com.union.insurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnionProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnionProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/union/insurance/UnionProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.union.insurance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnionProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
